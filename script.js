bb9351b980bde8f808ed141f0feb6f970074936b
veresoora <lahanabira@gmail.com>
2024-04-07 02:08:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("video").addEventListener("click", function () {
+  alert(
+    "Nikmati aksi memasak Cinnamonroll yang spektakuler! Selamat menonton!"
+  );
+});
+
+document.getElementById("gambar").addEventListener("click", function () {
+  alert(
+    "Selamat datang di dunia Cinnamonroll! Ini foto Cinnamonroll dengan gelembung!"
+  );
+});
+
+document.getElementById("audio").addEventListener("click", function () {
+  alert("Dengarkanlah! Nikmati suara lembut Cinnamonroll!");
+});
